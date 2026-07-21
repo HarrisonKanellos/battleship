b4df4c4c769d6452033f736a49f77e46e7c958a3
HarrisonKanellos <harrisonkanellos@gmail.com>
2026-07-21 10:51:37 +1000

    Test: gameboard for setting ship coordinates
    Assert errors are thrown for invalid arguments in setCoordinatesOf
    method call, including the following cases:
    - Unknown ship name
    - Number of coordinates not equal to target ships length
    - Coordinates out of gameboard range
    - Coordinates not in straight line

---
+++ src/tests/gameboard.test.js
@@ -0,0 +1,91 @@
+import { createGameboard } from "../models/gameboard.js";
+
+const gameboard = createGameboard();
+gameboard.initShips();
+
+describe("set coordinates of ship", () => {
+    test("doesn't accept unknown ship name", () => {
+        const ship = {
+            name: "unknown ship",
+            coordinates: ["B3", "C3", "D4"],
+        };
+        const error = new Error("Ship name must exist in ships array.");
+
+        expect(() => {
+            gameboard.setCoordinatesOf(ship.name, ship.coordinates);
+        }).toThrow(error);
+            
+    });
+
+    test("doesn't accept invalid number of coordinates", () => {
+        const ship = {
+            name: "destroyer",
+            coordinates: ["A3", "A4", "A5", "A6"],
+        };
+        const error = new Error(
+            "Length of coordinates array must be equal to length of ship."
+        );
+
+        expect(() => {
+            gameboard.setCoordinatesOf(ship.name, ship.coordinates);
+        }).toThrow(error);
+    });
+
+    test("doesn't accept coordinates not in range (letter)", () => {
+        const ship = {
+            name: "battleship",
+            coordinates: ["L3", "A4", "A5", "A6"],
+        };
+
+        expect(() => {
+            gameboard.setCoordinatesOf(ship.name, ship.coordinates);
+        }).toThrow(RangeError);
+    });
+
+    test("doesn't accept coordinates not in range (number)", () => {
+        const ship = {
+            name: "battleship",
+            coordinates: ["A3", "A11", "A5", "A6"],
+        };
+
+        expect(() => {
+            gameboard.setCoordinatesOf(ship.name, ship.coordinates);
+        }).toThrow(RangeError);
+    });
+
+    test("doesn't accept coordinates not in range (large number)", () => {
+        const ship = {
+            name: "battleship",
+            coordinates: ["A3", "A4", "A117", "A6"],
+        };
+
+        expect(() => {
+            gameboard.setCoordinatesOf(ship.name, ship.coordinates);
+        }).toThrow(RangeError);
+    });
+
+    test("doesn't accept coordinates not in straight line (letter)", () => {
+        const ship = {
+            name: "battleship",
+            coordinates: ["A3", "A4", "A5", "B6"],
+        };
+        const error = new Error("Coordinates must be in straight line.");
+
+        expect(() => {
+            gameboard.setCoordinatesOf(ship.name, ship.coordinates);
+        }).toThrow(error);
+
+    });
+
+    test("doesn't accept coordinates not in straight line (number)", () => {
+        const ship = {
+            name: "battleship",
+            coordinates: ["A3", "A4", "A6", "A5"],
+        };
+        const error = new Error("Coordinates must be in straight line.");
+
+        expect(() => {
+            gameboard.setCoordinatesOf(ship.name, ship.coordinates);
+        }).toThrow(error);
+    });
+});
