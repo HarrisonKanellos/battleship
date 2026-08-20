d1cf432a28d5becb013bb3e13ef559b724ab3089
HarrisonKanellos <harrisonkanellos@gmail.com>
2026-08-20 22:13:51 +1000

    Chore: make webpack configs use ESM, add required configs

--- webpack-config/webpack.common.js
+++ webpack-config/webpack.common.js
@@ -1,7 +1,11 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
     entry: "./src/index.js",
     output: {
         filename: "[name].bundle.js",
