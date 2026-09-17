bb5c32454ef8b05fa93b86b518f605ad1ff740a4
HarrisonKanellos <harrisonkanellos@gmail.com>
2026-09-17 23:15:19 +1000

    Model: define getRandomPlacement function of computerActions
    Allows computer player to get array of coordinates, representing
    a pseudo random placement of ship, based on passed length argument.

--- src/models/computerActions.js
+++ src/models/computerActions.js
@@ -1,3 +1,5 @@
+import { coordinatesInRange } from "../helpers/gameboardChecks.js";
+
 const getRandomCoordinate = () => {
     const charCodeA = "A".charCodeAt(0);
 
@@ -10,4 +12,58 @@ const getRandomCoordinate = () => {
     return randomCoordinate;
 };
 
-export { getRandomCoordinate };
+const getRandomPlacement = (length) => {
+    const startCoordinate = getRandomCoordinate();
+    const orientation = getRandomOrientation();
+    let coordinates = [];
+    
+    let currColumnChar = startCoordinate.at(0);
+    let currColumnCharCode = currColumnChar.charCodeAt(0);
+    let currRowNum = Number(startCoordinate.slice(1));
+
+    // Add first coordinate to array
+    coordinates[0] = startCoordinate;
+
+    // Generate more coordinates based on orientation and length
+    let currCoordinate;
+    for (let i = 1; i < length; i++) {
+        if (orientation === "horizontal") {
+            currColumnChar = String.fromCharCode(++currColumnCharCode);
+            currCoordinate = currColumnChar.concat(currRowNum);
+            coordinates[i] = currCoordinate;
+        } else {
+            ++currRowNum;
+            currCoordinate = currColumnChar.concat(currRowNum);
+            coordinates[i] = currCoordinate
+        }
+    }
+
+    let isInRange = coordinatesInRange(coordinates);
+    while (!isInRange) {
+        coordinates = coordinates.map((coordinate) => {
+            let columnChar = coordinate.at(0);
+            let columnCharCode = columnChar.charCodeAt(0);
+            let rowNum = Number(coordinate.slice(1));
+
+            let newCoordinate;
+            if (orientation === "horizontal") {
+                columnChar = String.fromCharCode(--columnCharCode);
+                newCoordinate = columnChar.concat(rowNum);
+            } else {
+                --rowNum;
+                newCoordinate = columnChar.concat(rowNum);
+            }
+            return newCoordinate;
+        });
+
+        isInRange = coordinatesInRange(coordinates);
+    }
+
+    return coordinates;
+}; 
+
+const getRandomOrientation = () => {
+    return Math.random() < 0.5 ? "horizontal" : "vertical";
+};
+
+export { getRandomCoordinate, getRandomPlacement };
