b52fc9e8f2806c0572100a0c4a100b186cfdef0c
HarrisonKanellos <harrisonkanellos@gmail.com>
2026-09-18 23:45:13 +1000

    Model: implement getAdjacentCoordinate as computer action

--- src/models/computerActions.js
+++ src/models/computerActions.js
@@ -12,11 +12,47 @@ const getRandomCoordinate = () => {
     return randomCoordinate;
 };
 
+const getAdjacentCoordinate = (coordinate, usedDirections = []) => {
+    let randomDirection;
+    let isUniqueDirection = false;
+    do {
+        randomDirection = getRandomDirection();
+
+        if (!usedDirections.includes(randomDirection)) {
+            usedDirections.push(randomDirection);
+            isUniqueDirection = true;
+        }
+    } while (!isUniqueDirection);
+
+    let columnCharCode = coordinate.at(0).charCodeAt(0);
+    let rowNum = Number(coordinate.slice(1));
+    switch (randomDirection) {
+        case "up":
+            rowNum--;
+            break;
+        case "right":
+            columnCharCode++;
+            break;
+        case "down":
+            rowNum++;
+            break;
+        case "left":
+            columnCharCode--;
+    }
+    const adjacentCoordinate =
+        String.fromCharCode(columnCharCode).concat(rowNum);
+
+    if (coordinatesInRange([adjacentCoordinate])) {
+        return adjacentCoordinate;
+    }
+    return getAdjacentCoordinate(coordinate, usedDirections);
+};
+
 const getRandomPlacement = (length) => {
     const startCoordinate = getRandomCoordinate();
     const orientation = getRandomOrientation();
     let coordinates = [];
-    
+
     let currColumnChar = startCoordinate.at(0);
     let currColumnCharCode = currColumnChar.charCodeAt(0);
     let currRowNum = Number(startCoordinate.slice(1));
@@ -34,7 +70,7 @@ const getRandomPlacement = (length) => {
         } else {
             ++currRowNum;
             currCoordinate = currColumnChar.concat(currRowNum);
-            coordinates[i] = currCoordinate
+            coordinates[i] = currCoordinate;
         }
     }
 
@@ -60,10 +96,24 @@ const getRandomPlacement = (length) => {
     }
 
     return coordinates;
-}; 
+};
 
 const getRandomOrientation = () => {
     return Math.random() < 0.5 ? "horizontal" : "vertical";
 };
 
-export { getRandomCoordinate, getRandomPlacement };
+const getRandomDirection = () => {
+    const randomNum = Math.random();
+
+    if (randomNum < 0.25) {
+        return "up";
+    } else if (randomNum < 0.5) {
+        return "right";
+    } else if (randomNum < 0.75) {
+        return "down";
+    } else {
+        return "left";
+    }
+};
+
+export { getRandomCoordinate, getRandomPlacement, getAdjacentCoordinate };
